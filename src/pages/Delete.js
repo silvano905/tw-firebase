d221a9ce727bdb7860541839292cb7c324c4f510
unknown <silvanovaldez53@outlook.com>
2023-02-14 13:17:27 -0600

    fdj

--- src/pages/Delete.js
+++ src/pages/Delete.js
@@ -1,17 +1,32 @@
 import React, {useState, useEffect, useRef} from 'react';
 
-
-
+import CardMedia from '@mui/material/CardMedia';
+import Card from '@mui/material/Card';
 function Delete() {
 
 
         return (
-
-                <video>
-                    <source src="https://d3sog3sqr61u3b.cloudfront.net/popita2.mp4" type="video/mp4"/>
-                </video>
+            <>
+                <Card sx={{ display: 'flex', margin: 5 }}>
+                    <CardMedia
+                        component="video"
+                        image='https://d3sog3sqr61u3b.cloudfront.net/popita3.mp4'
+                        title="Teen creepshots"
+                        controls
+                    />
+                </Card>
+                <Card sx={{ display: 'flex', margin: 5 }}>
+                    <CardMedia
+                        component="video"
+                        image='https://d3sog3sqr61u3b.cloudfront.net/popita2.mp4'
+                        title="Teen creepshots"
+                        controls
+                    />
+                </Card>
+            </>
 
         )
+
 
 
 }
